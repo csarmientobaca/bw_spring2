b410b765fac79bf10e18c4896ef1572fa580cfa3
CARLO <CARLO@192.168.1.2>
2023-04-26 09:55:43 +0200

    starter

---
+++ bw_spring2/src/main/java/it/sarmientoB/bw_spring2/BwSpring2Application.java
@@ -0,0 +1,13 @@
+package it.sarmientoB.bw_spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwSpring2Application.class, args);
+	}
+
+}
---
+++ bw_spring2/src/test/java/it/sarmientoB/bw_spring2/BwSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sarmientoB.bw_spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
